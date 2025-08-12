1a1daf9eaf9ecbaaabfc39ddf1ec3f2e9a4c7901
Kadir-Hali <kadirhali07@gmail.com>
2025-08-12 21:37:38 +0300

    Add project files.

---
+++ CQRSDemoProjectNight/Context/DemoContext.cs
@@ -0,0 +1,17 @@
+﻿using CQRSDemoProjectNight.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSDemoProjectNight.Context
+{
+    public class DemoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; Initial Catalog = DemoCQRSNightDb; Integrated Security = true; Trust Server Certificate = true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
